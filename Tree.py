1108df28c9cab034cdc506bbd808fea7f45fb9ad
Stef-aap <stef.mientki@gmail.com>
2022-10-20 10:40:21 +0200

    Add files via upload

---
+++ Tree.py
@@ -0,0 +1,34 @@
+# this is comment
+# stop alle tekst in Tree.txt en run Jupyter  Generate_Index_html_02
+
+#def Node ( Name = "Installatie XXX Energie" ) :
+def Installatie_XXX_Energie :
+  a = "Eigen EnergieBedrijf5"          #EB5
+  a = "Eigen EnergieBedrijf5"          #EB6
+  a = "Eigen EnergieBedrijf5"          #EB7
+
+def Node ( Name = "Installatie Energie" ) :
+  a = "Eigen EnergieBedrijf2.pdf"     #EB7
+  a = "Eigen EnergieBedrijf2.pdf"     #EB7
+  a = "Eigen EnergieBedrijf2.pdf"     #EB7
+
+  #def Node ( Name = "Tweede Level" ) :
+  def Tweede_Level :
+    h = "https://www.zonatlas.nl/start/"     #Dak Potentieel
+    X = "https://www.trouw.nl/duurzaamheid-economie/we-gaan-zonnestroom-verkopen-aan-de-buren-met-dank-aan-brussel~b990f779/"   #Vincent verkoopt stroom
+    a = "Eigen EnergieBedrijf2.pdf"          #EB7
+
+  def Node ( Name = "Tweede Level" ) :
+    a = "Eigen EnergieBedrijf2.pdf"          #EB7
+    a = "Eigen EnergieBedrijf2.pdf"          #EB7
+    a = "Eigen EnergieBedrijf2.pdf"          #EB7
+
+a = Installatie_Energie/Energiewet_nov21_toelichting
+h = "https://www.zonatlas.nl/start/"     #Dak Potentieel
+
+def Node ( Name = "Niet Tweede Level" ) :
+  a = "Eigen EnergieBedrijf5"          #EB5
+
+def Node ( Name = "Eerste Level ") :
+  a = "Eigen EnergieBedrijf5"          #EB5
+  a = "Eigen EnergieBedrijf5"          #EB5
